d534ff4063022c1987e95523725fbabe5729a114
Stacey Vanyo <sbvanyo@gmail.com>
2023-11-28 15:53:42 -0600

    create game form + functionality

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,5 +7,30 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getGameTypes = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/gametypes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getGames };
+export { getGames, createGame, getGameTypes };
